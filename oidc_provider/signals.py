30d0461b0620e9b99fa7412274a7020f1d1839f2
developerfromjokela <developerfromjokela@gmail.com>
2022-06-13 19:47:24 +0300

    Bugfix

--- oidc_provider/signals.py
+++ oidc_provider/signals.py
@@ -2,5 +2,10 @@
 from django.dispatch import Signal
 
 
-user_accept_consent = Signal(providing_args=['user', 'client', 'scope'])
-user_decline_consent = Signal(providing_args=['user', 'client', 'scope'])
+try:
+    user_accept_consent = Signal(providing_args=['user', 'client', 'scope'])
+    user_decline_consent = Signal(providing_args=['user', 'client', 'scope'])
+except TypeError:
+    # Starting from django 4, signals don't accept providing arguments
+    user_accept_consent = Signal()
+    user_decline_consent = Signal()
